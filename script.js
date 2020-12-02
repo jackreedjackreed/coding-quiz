42570b9f01dcb098b7a11fa964f7338078931644
J-Boogie <jackr353@gmail.com>
2020-12-02 11:12:12 -0600

    link bootstrap/jQuery

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+//    V A R I A B L E S   //
+
+// user score
+//   var userScore = 0
+// questions array
+
+// timer starts at 60
+
+// ---------------------------------------------- //
+
 // start game function
+function startGame() {
+    // user score
+    var userScore = 0;
+    // starts the time
+    function startTime() {
+        // s t a r t   the   t i m e 
+
+    }
+    // hides the welcome page
+    
+    // calls for first question 
+
+
+}
+    
 
 // start timer function
     // subtract time when wrong answer
 
-// user score
+
 
 // present question and answers
 
