42716d4782de528da89758805aca9fc27a0a19f2
J-Boogie <jackr353@gmail.com>
2020-12-02 11:03:28 -0600

    rough js pseudo

---
+++ script.js
@@ -0,0 +1,24 @@
+// start game function
+
+// start timer function
+    // subtract time when wrong answer
+
+// user score
+
+// present question and answers
+
+// user answers a question
+
+    // new question
+
+    // IF WRONG, subtract time from timer
+
+// when no more questions or time = 0
+
+    // game over function
+
+    // display high scores
+
+        // add new names
+
+        // clear names
